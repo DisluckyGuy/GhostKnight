00e23a9291bbd3772c21fa54d0f3bbabd2424c7c
UnluckyGithubUser <mostafa.ahmed.saied.2010@gmail.com>
2023-09-24 14:35:51 +0300

    Corrected the project's name

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/parkour")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/ghostKnight")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
